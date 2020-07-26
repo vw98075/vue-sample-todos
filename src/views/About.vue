078f191f9939eb304157bac06a2e474f6855f133
vwu98075 <vwu98034@lycos.com>
2020-07-26 14:54:02 -0700

    2nd commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h3>This is an about page</h3>
+    <p>This is a Todo List app v1.0.0. It is for refreshing Vue after a few years absence.</p>
+  </div>
+</template>
